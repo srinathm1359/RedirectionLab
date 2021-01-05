52d83a3155a11a97471945beaa97b631172e1a85
srinathstuycs <smahankali10@stuy.edu>
2021-01-05 06:20:37 -0500

    Added testVowel()

--- PigLatin.java
+++ PigLatin.java
@@ -1,6 +1,11 @@
 import java.util.*;
 public class PigLatin{
-  
+  public static boolean testVowel(char a){
+    return (a =='a'||a =='e'||a =='i'||a =='o'||a =='u');
+  }
+  public static String pigLatinSimple(String s){
+    return "Hello";
+  }
   public static void main(String[] args){
 
   }
