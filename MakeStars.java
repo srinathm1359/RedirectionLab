ebeebf437383564d925646a37e06a7767cada4bd
srinathstuycs <smahankali10@stuy.edu>
2021-01-05 06:08:15 -0500

    Completed main method and tested

--- MakeStars.java
+++ MakeStars.java
@@ -18,7 +18,15 @@ public class MakeStars{
     Scanner n = new Scanner( System.in );
     //use hasNextLine()/nextLine() to loop over
     //all of the data
-
+    while (n.hasNextLine()) {
+      Scanner scan = new Scanner(n.nextLine());
+      while (scan.hasNext()) {
+        String word = scan.next();
+        System.out.print(wordToStars(word));
+        System.out.print(" ");
+      }
+      System.out.println();
+    }
     //If you want to read the input word by word
     //this can be replaced with hasNext() and next()
   }
